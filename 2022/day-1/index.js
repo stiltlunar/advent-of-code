9667f68d2186218142636d9be8c4784c84ab2875
Cody Meuer <93593334+stiltlunar@users.noreply.github.com>
2022-12-01 14:35:18 -0600

    part 1

--- 2022/day-1/index.js
+++ 2022/day-1/index.js
@@ -1,3 +1,23 @@
 const fs = require('fs')
 
-const text = fs.readFileSync('./input.txt', {encoding: 'utf8'})
+const input = fs.readFileSync('./input.txt', {encoding: 'utf8'})
+
+const elves = input.split('\n\n')
+
+function calcCalories(elf) {
+  const calories = elf.split('\n')
+  return calories.reduce((acc, curr) => parseInt(acc) + parseInt(curr))
+}
+
+const calories = elves.map(elf => parseInt(calcCalories(elf)))
+calories.sort((a, b) => {
+  if (a < b) {
+    return -1
+  }
+  if (a > b) {
+    return 1
+  }
+  return 0
+})
+
+console.log(calories[calories.length - 1]);
